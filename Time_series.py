2b9b5d6f6f7d54ddd51a35c7b15ea41021afb86b
walnutwater <alexander.dick@strath.ac.uk>
2013-12-13 09:54:22 +0000

    Added Doc Strings

--- Time_series.py
+++ Time_series.py
@@ -1,9 +1,12 @@
-
 class TimeSeries(object):
+
+    
+    Takes a par'''
     def __init__(self, data):
         self.data = data
     
     def get(self, x):
+        '''Find the corresponding y-value when given an x-value'''
         for (xi,yi) in self.data:
             if xi == x:
                 return yi
@@ -11,6 +14,7 @@ class TimeSeries(object):
         raise Exception("Didn't find the value")
     
     def view(self):
+        """function to view the data, but empty function right now"""
         pass
     
 
@@ -19,13 +23,14 @@ class StepFunctionTimeSeries(TimeSeries):
         '''Given an X value, get the corresponding Y value.
         
         Uses step interpolation (gets the Y value of the nearest X point)'''
-        
+        '''note that this loop does not cover edge cases when points which are out of range of the data'''
         closest_point = None
         for (xi, yi) in self.data:
             if closest_point is None:
                 closest_point = (xi, yi)
             else:
                 cx, cy = closest_point
+                '''Compares for nearest point'''
                 if abs(xi-x) < abs(cx-x):
                     closest_point = (xi, yi)
         return closest_point[1]
@@ -33,11 +38,15 @@ class StepFunctionTimeSeries(TimeSeries):
 
 
 class LinearTimeSeries(TimeSeries):
+        '''Uses linear interpolation in the get() method of a TimeSeries'''
     def __init__(self, data):
+
         TimeSeries.__init__(self, data)
         self.data.sort()
     
     def get(self, x):
+        '''Method to find two closest data points to x and linearly
+interpolate between them to estimate the y-value'''
         # if it's out of range to the left,
         # return the first value
         if x < self.data[0][0]:
@@ -45,6 +54,7 @@ class LinearTimeSeries(TimeSeries):
         # if it's out of range to the right,
         # return the last value
         elif x > self.data[-1][0]:
+            # [-1] means return last value 
             return self.data[-1][1]
         # otherwise, it's within the range
         for (n, (xi, yi)) in enumerate(self.data):
